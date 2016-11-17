630810f89220b1f87c57e0eca4cfa321043c1c1d
Angristan <stanislas.lange@protonmail.com>
2016-11-17 23:06:52 +0100

    Just changin' the variable
    cuz ima nice person.
    
    https://twitter.com/lanodan/status/799372593723805696

--- check.sh
+++ check.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-IP="IP or domain"
+host="IP or domain"
 mail="the email adress that will receive mails"
-status=$(fping $IP)
+status=$(fping $host)
 status=$(echo $status | awk '{print $3}')
-if [ -e /tmp/status-$IP.txt ]
+if [ -e /tmp/status-$host.txt ]
   then
-    laststatus=$(cat /tmp/status-$IP.txt)
+    laststatus=$(cat /tmp/status-$host.txt)
     if [[ $laststatus != $status ]]
       then
-        echo $status > /tmp/status-$IP.txt
+        echo $status > /tmp/status-$host.txt
         if [[ $status = "alive" ]]
           then
-            echo "Hi, the server $IP is back up." | mail -s "The server $IP is UP !" $mail
+            echo "Hi, the server $host is back up." | mail -s "The server $host is UP !" $mail
         else
-          echo "Hi, the server $IP is down." | mail -s "he server $IP is DOWN !" $mail
+          echo "Hi, the server $host is down." | mail -s "he server $host is DOWN !" $mail
         fi
     fi
 else
-  echo $status > /tmp/status-$IP.txt
+  echo $status > /tmp/status-$host.txt
 fi
